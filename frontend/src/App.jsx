62f80c837dc095c345b0e50d575de617d4866bff
efeborasaglam <efebora.saglam@hotmail.com>
2026-08-04 15:10:02 +0200

    doctors page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import Login from './pages/Login.jsx';
+import MyAppointments from './pages/MyAppointments.jsx';
+import Doctors from './pages/Doctors.jsx';
+import MyProfile from './pages/MyProfile.jsx';
+import Appointment from './pages/Appointment.jsx';
+import NavBar from './components/NavBar.jsx';
+import Footer from './components/Footer.jsx';
+
+const App = () => {
+    return (
+        <div className={'mx-4 sm:mx-[10%]'}>
+            <NavBar />
+            <Routes>
+                <Route path={'/'} element={<Home />} />
+                <Route path={'/about'} element={<About />} />
+                <Route path={'/contact'} element={<Contact />} />
+                <Route path={'/login'} element={<Login />} />
+                <Route path={'/MyAppointments'} element={<MyAppointments />} />
+                <Route path={'/doctors'} element={<Doctors />} />
+                <Route path={'/doctors/:speciality'} element={<Doctors />} />
+                <Route path={'/my-profile'} element={<MyProfile />} />
+                <Route path={'/appointment/:docId'} element={<Appointment />} />
+            </Routes>
+            <Footer />
+        </div>
+    );
+};
+export default App;
